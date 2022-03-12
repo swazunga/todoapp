3e3f01f783f6aa48738dcf601ebce75db4d3e491
Michael Kendrick <michael@locumsmart.net>
2022-03-12 00:12:39 -0600

    didn't get far

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var form = document.querySelector(".form");
+
+function submitItem() {
+  console.log("click");
+}
+
+form.addEventListener("click", submitItem);
+
+// form.addEventListener("keyup", function (event) {
+//   if (event.code === 13) {
+
+//     console.log("click");
+//   }
+// });
