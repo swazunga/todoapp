d22b9a307204865d179e787db8d694201e63bbd1
Michael Kendrick <michael@locumsmart.net>
2022-03-12 16:36:26 -0600

    trying js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
+var formEl = document.querySelector(".formDiv");
 var form = document.querySelector(".form");
+var todoList = document.querySelector(".todoList");
+var item = document.createElement("li");
 
-function submitItem() {
-  console.log("click");
+items = [];
+
+function submitItem(event) {
+  event.preventDefault();
+  // for (var i = 0; i < item.length; i++) {
+  todoList.appendChild(item);
+  item.classList.add("item");
+  item.textContent = form.value;
+  items.push(form.value);
+  localStorage.setItem("items", JSON.stringify(items));
+  console.log(items);
+  // }
 }
 
-form.addEventListener("click", submitItem);
+formEl.addEventListener("submit", submitItem);
 
 // form.addEventListener("keyup", function (event) {
 //   if (event.code === 13) {
