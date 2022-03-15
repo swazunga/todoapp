f1073dda7b49d8fa14795b37e304778f039c93a5
Michael Kendrick <michael@locumsmart.net>
2022-03-15 14:29:24 -0500

    stopping point, need to figure out checkbox interaction

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,54 @@ var form = document.querySelector(".form");
 var todoList = document.querySelector(".todoList");
 var dragP = document.querySelector(".drag");
 var tickerEl = document.querySelector(".noItems");
+var clearEl = document.querySelector(".clearBtn");
 
 items = JSON.parse(localStorage.getItem("items")) || [];
+
+function loadItems() {
+  console.log("loaded");
+  JSON.parse(localStorage.getItem(items));
+  console.log(parseInt(tickerEl.innerHTML) + items.length);
+
+  tickerEl.textContent = parseInt(tickerEl.innerHTML) + items.length;
+
+  for (var i = 0; i < items.length; i++) {
+    var previousItemEl = document.createElement("li");
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    var btnX = document.createElement("button");
+    var ticker = tickerEl.innerHTML;
+    todoList.prepend(btnX);
+    todoList.prepend(previousItemEl);
+    todoList.prepend(checkbox);
+    btnX.id = "btnX";
+    previousItemEl.textContent = items[i].value;
+    previousItemEl.classList = "item";
+    checkbox.classList = "check";
+    btnX.classList = "btnX";
+    btnX.textContent = "X";
+    if (items[i].isChecked === true) {
+      previousItemEl.classList = "strikethrough";
+    }
+  }
+  function checkOff(event) {
+    if (event.target.checked) {
+      ticker--;
+      tickerEl.textContent = ticker;
+      previousItemEl.classList.add("strikethrough");
+      console.log(itemObj.isChecked);
+      console.log("check!");
+    }
+    if (!event.target.checked) {
+      ticker++;
+      tickerEl.textContent = ticker;
+      previousItemEl.classList.remove("strikethrough");
+      console.log(itemObj.isChecked);
+      console.log("uncheck!");
+    }
+  }
+  todoList.addEventListener("change", checkOff);
+}
 
 function submitItem(event) {
   event.preventDefault();
@@ -25,7 +71,11 @@ function submitItem(event) {
     checkbox.classList = "check";
     btnX.textContent = "X";
     itemEl.textContent = form.value.trim();
-    items.push(form.value.trim());
+    itemObj = {
+      isChecked: false,
+      value: form.value.trim(),
+    };
+    items.push(itemObj);
     localStorage.setItem("items", JSON.stringify(items));
     ticker = tickerEl.textContent;
     ticker++;
@@ -36,26 +86,32 @@ function submitItem(event) {
     itemEl.remove();
     btnX.remove();
     checkbox.remove();
+    items.remove();
     ticker--;
     tickerEl.textContent = ticker;
     console.log("X");
   });
   function checkOff(event) {
     if (event.target.checked) {
-      console.log(event.target);
       ticker--;
       tickerEl.textContent = ticker;
+      itemObj.isChecked = true;
       itemEl.classList.add("strikethrough");
-      console.log("check!");
     }
     if (!event.target.checked) {
-      console.log("uncheck!");
-      itemEl.classList.remove("strikethrough");
       ticker++;
       tickerEl.textContent = ticker;
+      itemObj.isChecked = false;
+      itemEl.classList.remove("strikethrough");
     }
   }
   todoList.addEventListener("change", checkOff);
 }
 
+function clearCompleted() {
+  console.log("clear");
+}
+
 formEl.addEventListener("submit", submitItem);
+clearEl.addEventListener("click", clearCompleted);
+loadItems();
