b33d9a59be1da2cc58f2c4d530b8c5e947566cea
Michael Kendrick <michael@locumsmart.net>
2022-03-14 22:32:50 -0500

    lists working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,61 @@
 var formEl = document.querySelector(".formDiv");
 var form = document.querySelector(".form");
 var todoList = document.querySelector(".todoList");
-var item = document.createElement("li");
+var dragP = document.querySelector(".drag");
+var tickerEl = document.querySelector(".noItems");
 
-items = [];
+items = JSON.parse(localStorage.getItem("items")) || [];
 
 function submitItem(event) {
   event.preventDefault();
-  // for (var i = 0; i < item.length; i++) {
-  todoList.appendChild(item);
-  item.classList.add("item");
-  item.textContent = form.value;
-  items.push(form.value);
-  localStorage.setItem("items", JSON.stringify(items));
-  console.log(items);
-  // }
+  if (form.value === "") {
+    alert("You must write something!");
+  } else {
+    var itemEl = document.createElement("li");
+    var checkbox = document.createElement("INPUT");
+    var btnX = document.createElement("button");
+    btnX.id = "btnX";
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.id = "checkbox";
+    todoList.prepend(btnX);
+    todoList.prepend(itemEl);
+    todoList.prepend(checkbox);
+    btnX.classList = "btnX";
+    itemEl.classList = "item";
+    checkbox.classList = "check";
+    btnX.textContent = "X";
+    itemEl.textContent = form.value.trim();
+    items.push(form.value.trim());
+    localStorage.setItem("items", JSON.stringify(items));
+    ticker = tickerEl.textContent;
+    ticker++;
+    tickerEl.textContent = ticker;
+  }
+  form.value = "";
+  document.getElementById("btnX").addEventListener("click", function () {
+    itemEl.remove();
+    btnX.remove();
+    checkbox.remove();
+    ticker--;
+    tickerEl.textContent = ticker;
+    console.log("X");
+  });
+}
+
+function checkOff(event) {
+  if (event.target.checked) {
+    console.log(ticker);
+    ticker--;
+    tickerEl.textContent = ticker;
+    console.log("check!");
+  }
+  if (!event.target.checked) {
+    console.log("uncheck!");
+    ticker++;
+    tickerEl.textContent = ticker;
+  }
 }
 
 formEl.addEventListener("submit", submitItem);
 
-// form.addEventListener("keyup", function (event) {
-//   if (event.code === 13) {
-
-//     console.log("click");
-//   }
-// });
+todoList.addEventListener("change", checkOff);
